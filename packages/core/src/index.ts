a7d2001f2b6e51d1b1d6b1e4397fa72b2bfb9d0a
Alejandro Ortiz Corro <a.ortizcrr@gmail.com>
2026-07-09 19:49:51 -0600

    chore: centralize coding orchestration

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,25 +1,2 @@
 export { assertOneOf, isOneOf } from "./validation.js";
 export { escapeHtml, trimToUndefined } from "./strings.js";
-export {
-  EXECUTION_TOOL_SURFACES,
-  EXECUTION_WORKER_ROLES,
-  EXECUTION_WORKERS,
-  EXECUTION_REASONING_EFFORTS,
-  MODEL_TIERS,
-  OPENAI_EXECUTION_MODELS,
-  createExecutionRunPlan,
-  routeExecutionTask,
-} from "./agent-execution.js";
-export type {
-  ExecutionReasoningEffort,
-  ExecutionRoute,
-  ExecutionRunPlan,
-  ExecutionTaskSignal,
-  ExecutionToolSurface,
-  ExecutionWave,
-  ExecutionWorkerAssignment,
-  ExecutionWorkerProfile,
-  ExecutionWorkerRole,
-  ModelTier,
-  OpenAiExecutionModel,
-} from "./agent-execution.js";
